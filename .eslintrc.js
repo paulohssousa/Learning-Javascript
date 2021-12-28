7db35d62464f4bd7c725525d200ed5c9b1761b18
paulohssousa <paulohssousa@gmail.com>
2021-12-28 14:15:36 -0300

    end of chapter twogit add -A

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true,
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+        "react": {
+            "version": "latest",
+        }
+    }
+};
